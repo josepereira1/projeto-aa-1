459942c62093f1ab546ed1063843a30c67422c59
Ricardo Petronilho <rpetronilho98@gmail.com>
2020-04-27 22:31:12 +0100

    GMSWeb não minado.

--- tp7/A81744/GMSWeb/src/java/aa/IGMS.java
+++ tp7/A81744/GMSWeb/src/java/aa/IGMS.java
@@ -8,7 +8,7 @@ import org.orm.PersistentSession;
 public interface IGMS {
     
     void registerUser(User user, PersistentSession s) throws PersistentException, UserAlreadyExistsException;
-    public User getUser(String name, PersistentSession s)  throws PersistentException, UserNotExistsException;
+    public User getUser(String name, PersistentSession s) throws PersistentException, UserNotExistsException;
     Collection<Game> getUserGames(String userName, PersistentSession s) throws PersistentException, UserNotExistsException;
     boolean autenticateUser(String name, String password, PersistentSession s) throws PersistentException, UserNotExistsException;
     void addGameToUser(String username, Game game, PersistentSession persistentSession) throws PersistentException, UserNotExistsException;
